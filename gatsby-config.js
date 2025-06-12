080e5dae98ba23404a227f9f98cb15a8886f4dd6
Miroslav Evjak <miroslav.evjak@eset.com>
2025-06-12 09:57:30 +0200

    Update configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,39 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Nadeje2025`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+    siteMetadata: {
+        title: "Nadeje2011",
+        siteUrl: `https://www.yourdomain.tld`
+    },
+    plugins: [
+        "gatsby-plugin-less",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-transformer-json",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                "name": "data",
+                "path": `${__dirname}/src/data/`
+            },
+            __key: "data"
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                "name": "images",
+                "path": `${__dirname}/src/images/`
+            },
+            __key: "images"
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                "name": "pages",
+                "path": `${__dirname}/src/pages/`
+            },
+            __key: "pages"
+        }
+    ]
+};
