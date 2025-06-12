d458499cfb2b002020c0c4efc78daa4ce2f28158
Miroslav Evjak <miroslav.evjak@eset.com>
2025-06-12 11:27:32 +0200

    Base pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,17 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import Layout from "../components/layout";
 
 const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    return (
+        <Layout pageTitle="Stránka neexistuje">
+        <div>
+            <h1><span>404 - Stránka neexistuje</span></h1>
+            <p>Zkontrolujte, zda jste zadali požadovanou adresu správně.</p>
+            <p><Link to={'/'}>Zpět na úvodní stránku</Link></p>
+        </div>
+        </Layout>
+    )
 }
 
 export default NotFoundPage
-
-export const Head = () => <title>Not found</title>
