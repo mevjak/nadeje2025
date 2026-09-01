bcebfb4d58e20a582ba747d8de0b355f073262e7
Miroslav Evjak <miroslav.evjak@eset.com>
2026-09-01 08:56:32 +0200

    Update sponsors page for 2026 event, adjust content structure, and modify sponsor data

--- src/pages/sponzori.js
+++ src/pages/sponzori.js
@@ -23,38 +23,48 @@ const SponsorsPage = () => {
     return (
         <Layout pageTitle="Sponzoři">
             <div>
-                <h1><span>Partneři a sponzoři</span></h1>
+                <h1><span>Děkujeme sponzorům ročníku 2026</span></h1>
 
-                <h2 className="centered">Firmy a podnikatelé</h2>
-                <div className="row">
-                    {data.allSponsorsJson.edges.map(({node}) => {
-                        return (
-                            <Sponsor key={v4()} sponsor={node}/>
-                        )
-                    })}
-                </div>
-
-                <br/>
-
-                <h2 className="centered">Dárci</h2>
-
-                <p><i>Rodina Hübnerova, rodina Olsen, manželé Bubeníkovi, manželé Štěpánkovi, manželé Chládkovi,
-                    manželé Svárovští, manželé Fouňovi, Lenka Vurmová a Jiří Chlum, Petr Štěpánek ml., Iva Tejnská,
-                    Ema Dong, Laďka Štěpánková, paní Fidlerová, paní Zelenková, Simona Veverková, Alexandra Víchová,
-                    Šárka Petráňová, Ema Zábrodská, Romana Lakomá, pan Bajus, pan Patrman, Roman Sucharda,
-                    Jan Mysliveček, Radek Polák, Vladimír Enge, Robert Bubník, Zdeněk Vodička, Jan Tlapák,
-                    Miroslav Balatka, Milan Bělohradský, Stanislav Sluka, Jiří Zeman, Ondra Málek, Radek Duda,
-                    Michal Veverka, Miroslav Evják.</i></p>
+                <h2 className="centered">Firmy, podnikatelé a dárci</h2>
+                {/*<div className="row">*/}
+                {/*    {data.allSponsorsJson.edges.map(({node}) => {*/}
+                {/*        return (*/}
+                {/*            <Sponsor key={v4()} sponsor={node}/>*/}
+                {/*        )*/}
+                {/*    })}*/}
+                {/*</div>*/}
+                <p><i>
+                    Obec Jiřetín pod Bukovou, Obec Josefův Důl, Pan Mysliveček, Stavební společnost TERMIL, Rodina Petráňova a rodina Havlíkova z USA,
+                    MUDr. Karel Stuchlík, Tesařství Smutný, Aleš Zeman – zemní práce, Zewling s.r.o., Sportovní kroužek Albrechtík,
+                    Penzion Sněženka, Restaurace Špičák, CS PRO – TEC Praha s.r.o., Michal Veverka, Petr Štěpánek ml.,
+                    Laďka Štěpánková, Iva Tejnská, Manželé Štěpánkovi, Paní Charvátová, Věra Engeová, Kominictví Honza Pipek,
+                    Hübnerovi – JHR s.r.o., Manželé Bubníkovi, Martin Lukavec, Hospůdka a cukrárna U Voborníků, VESPO Štěpán Vejražka,
+                    Roman Sucharda, Penzion Alenka – Klauzovi, Lenka Vurmová a Jirka Chlum, Miroslav Evják, Paní Zelenková,
+                    Manželé Svárovští, Miroslav Jelínek, Zdeněk Vodička, Miroslav Balatka, Petr Tichý, Ríša Lurský, Rodina Štrynclova,
+                    Václav Fikar, Standa Sluka, Adam Rypl, Jiří Zeman, Kitl s.r.o.
+                </i></p>
 
                 <br/>
 
-                <h2 className="centered">Dobrovolníci</h2>
+                <h2 className="centered">Děkujeme všem, kteří přispěli do dražby</h2>
+
+                <p><i>
+                    ISR racing, Stavební společnost TERMIL, Zemní práce Aleš Zeman, Huť Marie, Petr Ševčík, Kateřina Bubeníková,
+                    Martin Fikar, Šárka Nováková, Ema Zábrodská.
+
+                </i></p>
+
+                <br/>
+
+                <h2 className="centered">Děkujeme dobrovolníkům, kteří pomohli s organizací</h2>
 
                 <p>Na organizaci posledního ročníku Běhu naděje se podíleli naši obětaví kamarádi dobrovolníci:</p>
 
-                <p><i>Zuzana Erlebachová, Honza Fikar ml., Adam Kotus, Aranka Rosíková, Radka Kittlová, Laďka
-                    Štěpánková, Petr a Helena Štěpánkovi, Petra Keslová, Jana Petráňová, Šárka Petráňová, Míša a Martin
-                    Bouškovi, Michal Rovenský, Zuzka Petráňová, Radek a Bára Petráňovi, Petr Jelínek.</i></p>
+                <p><i>
+                    Josef Kucín, Zuzka Erlebachová, Petr Jelínek, Aranka Rosíková, Radka Kittelová, Petr a Helča Štěpánkovi,
+                    Andy a David Januškovi, Jana Petráňová, Laďka Štěpánková, Michal Rovenský, Adam Kotus, Martin Fikar,
+                    Honza Fikar ml., Míša Boušková, Šárka Nováková, Alenka Ducháčková, Míša Štrynclová.
+                </i></p>
 
             </div>
         </Layout>
